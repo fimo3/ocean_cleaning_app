37dbf3262a397644f57fbab6aca8ed3267b9b926
fimo3 <serafimkovacevic@gmail.com>
2024-04-12 17:58:05 +0300

    Profile

---
+++ app/(components)/ShoppingCartItems.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShoppingCartItems = () => {
+  return <div>ShoppingCartItems</div>;
+};
+
+export default ShoppingCartItems;
--- app/(components)/UserProfile.jsx
+++ app/(components)/UserProfile.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import ShoppingCartItems from "./ShoppingCartItems";
 
 const profile = () => {
   return (
-    <div>
-      <h2>User</h2>
-      <hr />
-      <h3>Shopping cart:</h3>
+    <div className="flex justify-center">
+      <div className="flex flex-col gap-3 w-1/2 bg-secondary rounded-lg mt-20">
+        <h2 id="card">User</h2>
+        <hr id="card" />
+        <h3 id="card">Shopping cart:</h3>
+        <ShoppingCartItems />
+        <ShoppingCartItems />
+        <ShoppingCartItems />
+        <ShoppingCartItems />
+      </div>
     </div>
   );
 };
