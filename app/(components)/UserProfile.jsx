f8b4b27966d12a59144a7f63f43f0957fde2c7a3
fimo3 <serafimkovacevic@gmail.com>
2024-04-14 19:26:16 +0300

    Profile+game

--- app/(components)/UserProfile.jsx
+++ app/(components)/UserProfile.jsx
@@ -6,16 +6,23 @@ const profile = () => {
   return (
     <div className="flex justify-center m-2">
       <div className="flex flex-col gap-3 w-1/2 bg-secondary hover:bg-secondary-hover rounded-lg mt-20">
-        <Image
-          src={UserPicture}
-          alt="Снимка :>"
-          className="m-3 object-cover w-20 h-20 rounded-3xl"
-        />
-        <div className="lg:grid grid-cols-2 xs:grid-cols-1">
-          <h2 id="card">Username</h2>
-          <a href="#">
-            <button className="p-3">Edit info</button>
-          </a>
+        <div className="lg:grid grid-cols-1 xl:grid-cols-3">
+          <Image
+            src={UserPicture}
+            alt="Снимка :>"
+            className="m-3 object-cover w-20 h-20 rounded-3xl"
+          />
+          <div className="grid grid-cols-1">
+            <h2 id="card">Username</h2>
+            <div>
+              <a href="#">
+                <button className="p-3">Edit info</button>
+              </a>
+              <a href="#">
+                <button className=" m-1 p-3">Log out</button>
+              </a>
+            </div>
+          </div>
         </div>
         <hr id="card" />
         <h3 id="card">Shopping cart:</h3>
--- app/Simulation/page.jsx
+++ app/Simulation/page.jsx
@@ -1,16 +1,19 @@
-import Script from "next/script";
+import Image from "next/image";
+import img1 from "@/public/kostenurka.png";
 const game = () => {
   return (
     <div>
       <h1 className="mt-5 ml-5">Game</h1>
       <hr className="text-default-text" />
       <link src="index.html" />
-      <iframe
-        width="1000"
-        height="1000"
-        src="https://fimo155.free.bg"
-        allowfullscreen
-      ></iframe>
+      <div className="lg:grid grid-cols-1 xl:grid-cols-2">
+        <div className="bg-secondary">
+          <a target="_blank" href="https://fimo155.free.bg/Tuesfest/game/">
+            <Image src={img1} alt="Снимка :>" className="p-4 w-auto" />
+          </a>
+        </div>
+        <h2>Click on the image to get to the game.</h2>
+      </div>
     </div>
   );
 };
