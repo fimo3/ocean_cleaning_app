563f52e0376b99256ebadb282755956013ecbe51
fimo3 <serafimkovacevic@gmail.com>
2024-04-15 16:38:48 +0300

    Login1

---
+++ app/(components)/LoginPage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="flex justify-around">
+      <form
+        className="flex flex-col gap-3 w-1/2 bg-secondary rounded-lg mt-20"
+        method="post"
+      >
+        <h3 id="card">Log in</h3>
+        <label>Username: </label>
+        <input id="username" name="username" type="text" required={true} />
+        <label>Password: </label>
+        <input id="password" name="password" required={true} />
+        <button>
+          <input type="submit" value="Log in" />
+        </button>
+        <p>Do not have a profile?</p>
+        <a href="#">Sign up</a>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
