18632ca0059922e2fb36190db6891678b535ac3c
fimo3 <serafimkovacevic@gmail.com>
2024-04-15 16:20:57 +0300

    member

--- app/(components)/Member.jsx
+++ app/(components)/Member.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import Image from "next/image";
+import img1 from "@/public/kostenurka.png";
 
 const Member = () => {
   return (
-    <div>
+    <div className="bg-bluey p-3 hover:bg-bluey-hover">
       <h3>Serafim Kovachevich</h3>
-      <p>
-        I am Serafim Kovachevic and I live in Sofia, Bulgaria.
-        <br />I like to code and think a lot about the bad things happening in
-        the world.
-      </p>
+      <div className="flex flex-row">
+        <Image src={img1} alt="Снимка :>" className="w-20 h-20 rounded-full" />
+        <p>
+          I am Serafim Kovachevic and I live in Sofia, Bulgaria.
+          <br />I like to code and think a lot about the bad things happening in
+          the world.
+        </p>
+      </div>
     </div>
   );
 };
