1dbef8e93c1a4524f9b5e2af8a24db4d4ddfa20e
fimo3 <serafimkovacevic@gmail.com>
2024-04-18 00:09:27 +0300

    backend1

--- app/api/Donations/route.js
+++ app/api/Donations/route.js
@@ -7,10 +7,18 @@ export async function POST(req) {
     const body = await req.json();
     const donationData = body.formData;
     await Donation.create(donationData);
-
     return NextResponse.json({ message: "Donation Created" }, { status: 201 });
   } catch (error) {
     console.log(error);
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const donations = await Donation.find();
+    return NextResponse.json({ donations }, { status: 200 });
+  } catch (error) {
     return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
 }
